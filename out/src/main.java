b8c60dbdb7d8451820f8efa29eb48fccbd701088
Henri <153419239+hent83722@users.noreply.github.com>
2025-06-25 21:36:30 +0200

    Update main.java

--- out/src/main.java
+++ out/src/main.java
@@ -1,1 +1,6 @@
-
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("jar test");
+    }
+}
+//this is just an example, you can add your code on the java file you want to compile here.
